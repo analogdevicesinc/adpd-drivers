b3354585f89bfa6f5319dd30540ab06016c48e99
andrestraker <andre.straker@analog.com>
2019-03-11 11:29:08 -0400

    Removing doxygen folder
    Removed doyxgen folder for example108.c.

--- Documentation/Doxygen-ADPDDrv/html/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "ADPD Drivers", "dir_ab95c7804c437351ab4938e945245a19.html", "dir_ab95c7804c437351ab4938e945245a19" ]
-];
--- Documentation/Doxygen-ADPDDrv/html/modules.js
+++
@@ -1,4 +0,0 @@
-var modules =
-[
-    [ "ADPD", "group___adpd___driver.html", "group___adpd___driver" ]
-];
